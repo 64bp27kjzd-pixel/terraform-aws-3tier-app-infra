bf49d0857f19831ef0ae1f3102b842aa19344595
64bp27kjzd-pixel <252131502+64bp27kjzd-pixel@users.noreply.github.com>
2026-05-09 13:50:59 +0900

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-github-actions-test"
-    key            = "dev/myapp/terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
